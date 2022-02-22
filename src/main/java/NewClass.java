cd4bc6b0f824a3aedf408063826d51a55b2a14d7
abdul <abdul@Abdulkadir>
2022-02-23 01:00:27 +0200

    deneme commit

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author abdul
- */
 public class NewClass {
-    
+    public static void main(String[] args) {
+        
+
+
+    }
 }
